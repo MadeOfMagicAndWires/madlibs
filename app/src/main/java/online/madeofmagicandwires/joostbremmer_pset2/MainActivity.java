416f5d92562fcffcbedc1f4d115eb25321a87c91
Joost Bremmer <contact@madeofmagicandwires.online>
2018-12-02 22:50:13 +0100

    Working dropdown options and submit button prototype

--- app/src/main/java/online/madeofmagicandwires/joostbremmer_pset2/MainActivity.java
+++ app/src/main/java/online/madeofmagicandwires/joostbremmer_pset2/MainActivity.java
@@ -1,32 +1,99 @@
 package online.madeofmagicandwires.joostbremmer_pset2;
 
+
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    /** Resource id of the Spinner dropdown **/
+    private final int dropDownId = R.id.dropdown;
+
+    /** list of text objects used by Spinner dropdown */
     private List<Text> texts;
+    /** TextAdapter used by Spinner dropdown **/
     private TextDropDownAdapter adapter;
 
+
+    /**
+     * On Click listener for the Submit button;
+     * gets the selected text from the dropdown and passes it on to the next Activity
+     * @see #initSubmitButton()
+     * @see TextDropDownAdapter
+     * @see #populateDropdown()
+     *
+     */
+    public class TextSubmitButton implements View.OnClickListener{
+
+        /**
+         * Called when a view has been clicked.
+         *
+         * @param v The view that was clicked.
+         */
+        @Override
+        public void onClick(View v) {
+            // TODO: Launch Second Activity
+            Spinner dropDown = findViewById(dropDownId);
+            Text txt = (Text) dropDown.getSelectedItem();
+            if(txt != null) {
+                Log.d("submit", "picked text " + txt.getTextTitle());
+            } else {
+                Snackbar sb = Snackbar.make(v, "Please select a text", Snackbar.LENGTH_SHORT);
+                sb.show();
+            }
+
+        }
+    }
+
+    /**
+     * Populates the text dropdown  Spinner with data and sets listeners
+     * @see TextReader
+     * @see TextDropDownAdapter
+     * @see #texts
+     */
+    public void populateDropdown() {
+        texts = TextReader.populateList(this, TextReader.TEXT_RES_IDS);
+        adapter = new TextDropDownAdapter(
+                this,
+                R.layout.dropdown_option,
+                texts,
+                R.layout.support_simple_spinner_dropdown_item);
+        Spinner dropdown = findViewById(dropDownId);
+        dropdown.setAdapter(adapter);
+    }
+
+    /**
+     * Sets relevant listeners to the submit button; dropdown must be initialsed and populated
+     * @see TextSubmitButton
+     * @see #populateDropdown()
+     *
+     */
+    public void initSubmitButton() {
+        Button submit = findViewById(R.id.madLibButton);
+        submit.setOnClickListener(new TextSubmitButton());
+    }
+
+    /**
+     * OnCreate method
+     * @param savedInstanceState bundle of saved data from last saved instance
+     * @see AppCompatActivity#onCreate(Bundle)
+     * @see AppCompatActivity#onSaveInstanceState(Bundle)
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         populateDropdown();
+        initSubmitButton();
     }
 
-    public void populateDropdown() {
-        texts = TextReader.populateList(this, TextReader.TEXT_RES_IDS);
-        adapter = new TextDropDownAdapter(
-                this,
-                R.layout.dropdown_option,
-                texts);
-        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
-        Spinner dropdown = findViewById(R.id.dropdown);
-        dropdown.setAdapter(adapter);
-    }
+
 }
--- app/src/main/java/online/madeofmagicandwires/joostbremmer_pset2/TextDropDownAdapter.java
+++ app/src/main/java/online/madeofmagicandwires/joostbremmer_pset2/TextDropDownAdapter.java
@@ -7,12 +7,33 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import java.util.List;
 
 
-public class TextDropDownAdapter extends ArrayAdapter<Text> {
+public class TextDropDownAdapter extends ArrayAdapter<Text> implements SpinnerAdapter {
 
+    private int resourceId;
+    private int dropDownResourceId;
+
+
+
+    /**
+     * Constructor setting the dropdown resource as well.
+     * @param context the current activity context
+     * @param resource the resource ID for a layout file to use for the dropdown
+     * @param objects the objects to be represented in the listView
+     * @param dropDownId the resource ID for a layout file to use for the dropdown options
+     */
+    public TextDropDownAdapter(@NonNull Context context,
+                               int resource,
+                               @NonNull List<Text> objects,
+                               int dropDownId) {
+        super(context, resource, objects);
+        this.resourceId = resource;
+        this.dropDownResourceId= dropDownId;
+    }
 
     /**
      * Constructor
@@ -24,15 +45,17 @@ public class TextDropDownAdapter extends ArrayAdapter<Text> {
      */
     public TextDropDownAdapter(@NonNull Context context,
                                 int resource,
-                                @NonNull List objects) {
+                                @NonNull List<Text> objects) {
         super(context, resource, objects);
+        this.resourceId = resource;
     }
+
 
     /**
      * Binds the data to a View to display
      *
      * @param position The position of the item
-     *                 within the adapter's data set of the item
+     *                 within the adapter's data set of the items
      *                 whose view we want
      * @param convertView The old view to reuse, if possible
      * @param parent The parent that this view will eventually be attached to;
@@ -46,20 +69,86 @@ public class TextDropDownAdapter extends ArrayAdapter<Text> {
                         @NonNull ViewGroup parent) {
         // get text object
         Text text = getItem(position);
+
         //create new convertView if necessary
         if(convertView == null) {
            convertView = LayoutInflater.from(getContext()).inflate(
-                   R.layout.dropdown_option,
+                   getResource(),
                    parent,
                    false);
         }
 
-        // populate convertView with the right data
+        // get textView
         TextView dropDownText = convertView.findViewById(R.id.dropdownOptionText);
-        dropDownText.setText(text.getTextTitle());
-        dropDownText.setTag(position);
+
+
+        // bind data to view
+        if(text != null) {
+            dropDownText.setText(text.getTextTitle());
+        }
+
 
         return convertView;
 
+    }
+
+    /**
+     * Creates a dropdown option view containing the data from a Text object
+     * @param position the position of the item in the Adapter's dataset
+     * @param convertView The old view to re-purpose if available, else null
+     * @param parent The parent view to which convertView is to be attached
+     * @return the resulting view
+     */
+    @Override
+    public View getDropDownView(int position,
+                                @Nullable View convertView,
+                                @NonNull ViewGroup parent) {
+
+        // create or convert new convertView
+        TextView dropDown;
+        if(convertView == null) {
+            dropDown = (TextView) LayoutInflater.from(getContext()).inflate(
+                    getmDropDownResource(),
+                    parent,
+                    false);
+        } else {
+            dropDown = (TextView) convertView;
+        }
+        // get Text data, bind it to view
+        Text text = getItem(position);
+        if(text != null) {
+            dropDown.setText(text.getTextTitle());
+        }
+
+        return dropDown;
+    }
+
+    /**
+     * Returns the Resource ID for the Layout to be inflated for DropDownViews
+     * @return int linking to the layout resource to be inflated
+     */
+    private int getmDropDownResource() {
+        return this.dropDownResourceId;
+    }
+
+    /**
+     * Returns the Resource ID for the Layout to be inflated for #getView()
+     * @return int linking to the layout resource to be inflated
+     */
+    private int getResource() {
+        return this.resourceId;
+    }
+
+
+    /**
+     * <p>Sets the layout resource to create the drop down views.</p>
+     *
+     * @param resource the layout resource defining the drop down views
+     * @see #getDropDownView(int, View, ViewGroup)
+     */
+    @Override
+    public void setDropDownViewResource(int resource) {
+        super.setDropDownViewResource(resource);
+        this.dropDownResourceId = resource;
     }
 }
